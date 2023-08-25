6aef29547c9732e4f09c87cb63bcaa93cd3c9979
Rúben Almeida <rubenalmeidasemiao@gmail.com>
2023-08-26 00:18:54 +0200

    Remove unecessary job

--- jobs_dsl/new-app.groovy
+++ jobs_dsl/new-app.groovy
@@ -1,15 +1,12 @@
-pipelineJob('new-app') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/RubenSemiao/websites.git')
-                    }
-                    branch('*/master')
-                }
-            }
-            lightweight()
-        }
-    }
-}
+import hudson.plugins.git.*;
+
+def scm = new GitSCM("git@github.com:dermeister0/Tests.git")
+scm.branches = [new BranchSpec("*/develop")];
+
+def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile")
+
+def parent = Jenkins.instance
+def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "New Job")
+job.definition = flowDefinition
+
+parent.reload()
