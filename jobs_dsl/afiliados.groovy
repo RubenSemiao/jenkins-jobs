eb44569a3ab19f2ba6923cf020dc6c0ba45249ee
Rúben Almeida <rubenalmeidasemiao@gmail.com>
2023-08-26 00:35:56 +0200

    Remove unecessary job

---
+++ jobs_dsl/afiliados.groovy
@@ -0,0 +1,12 @@
+import hudson.plugins.git.*;
+
+def scm = new GitSCM("https://github.com/RubenSemiao/jenkins-jobs.git")
+scm.branches = [new BranchSpec("*/master")];
+
+def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "afiliados/Jenkinsfile")
+
+def parent = Jenkins.instance
+def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Afiliados")
+job.definition = flowDefinition
+
+parent.reload()
